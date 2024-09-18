dd5530ea36c344efdb4bdc9a5a276ac4181b12b3
Yana Mavlenko <yuna18178@gmail.com>
2024-09-18 16:32:48 +0300

    theme added

--- src/index.js
+++ src/index.js
@@ -5,33 +5,33 @@ let secondNum = '';
 let operator = '';
 let temRes = '';
 
-const lightTheme = '/light.b152146b.css';
-const darkTheme = '/dark.d1ed3a19.css';
+// const lightTheme = '/light.b152146b.css';
+// const darkTheme = '/dark.d1ed3a19.css';
 
-document.addEventListener('DOMContentLoaded', setTheme)
-const theme = document.getElementById('theme');
+// document.addEventListener('DOMContentLoaded', setTheme)
+// const theme = document.getElementById('theme');
 
-const icon = document.querySelector('.material-icons');
-icon.addEventListener('click', changeTheme);
-function changeTheme() {
-    console.log(theme.getAttribute('href'));
-    if (theme.getAttribute('href') == lightTheme) {
-        theme.href = darkTheme;
-        icon.innerHTML = 'brightness_2';
-        localStorage.setItem('theme', darkTheme);
-        localStorage.setItem('icon', 'brightness_2');
-    } else {
-        theme.href = lightTheme;
-        icon.innerHTML = 'brightness_5';
-        localStorage.setItem('theme', lightTheme);
-        localStorage.setItem('icon', 'brightness_5');
-    }
-}
+// const icon = document.querySelector('.material-icons');
+// icon.addEventListener('click', changeTheme);
+// function changeTheme() {
+//     console.log(theme.getAttribute('href'));
+//     if (theme.getAttribute('href') == lightTheme) {
+//         theme.href = darkTheme;
+//         icon.innerHTML = 'brightness_2';
+//         localStorage.setItem('theme', darkTheme);
+//         localStorage.setItem('icon', 'brightness_2');
+//     } else {
+//         theme.href = lightTheme;
+//         icon.innerHTML = 'brightness_5';
+//         localStorage.setItem('theme', lightTheme);
+//         localStorage.setItem('icon', 'brightness_5');
+//     }
+// }
 
-function setTheme() {
-  theme.href = localStorage.getItem('theme') || 'css/light-theme.css';
-  icon.innerHTML = localStorage.getItem('icon') || 'light_mode';
-}
+// function setTheme() {
+//   theme.href = localStorage.getItem('theme') || 'css/light-theme.css';
+//   icon.innerHTML = localStorage.getItem('icon') || 'light_mode';
+// }
 
 const btnNum = document.querySelectorAll('[data-type="number"]');
 btnNum.forEach((el) =>
